7424b305038ac16b8799cbedcd3ee2fcae86fb00
eswar-7116 <eswardudi06@gmail.com>
2025-09-14 14:42:46 +0530

    feat: add Navbar and Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+import About from "@/components/layout/About";
+
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <div className="fixed top-0 left-1/2 -translate-x-1/2 w-1/2 h-1/2 rounded-full bg-blue-500 blur-[200px] opacity-40 animate-ball-expand" />
-      <div className="sticky top-0 z-10 p-0.5">
-        <nav className="rounded-full p-4 m-4 bg-transparent/30 backdrop-blur-md border border-white/20 shadow-md shadow-black">
-          <span className="text-2xl font-semibold">Eswar Dudi</span>
-        </nav>
-      </div>
+    <main className="w-full h-full px-20">
+      {/* About */}
+      <About />
     </main>
   );
 }
---
+++ src/components/layout/About.tsx
@@ -0,0 +1,25 @@
+import lobster from "@/fonts/lobster";
+
+export default function About() {
+  return (
+    <>
+      <div className="w-full p-10 flex flex-col items-center justify-center">
+        <div>
+          <h1 className={`text-7xl ${lobster.className}`}>
+            Hello👋,
+          </h1>
+          <div className="mt-5 flex items-center text-7xl font-semibold">
+            I'm&nbsp;<span className="text-blue-700">Eswar</span>.
+          </div>
+        </div>
+      </div>
+      
+      <div className="text-xl max-w-3xl mx-auto text-center leading-relaxed">
+        <p>
+          I am <b>Eswar Dudi</b>, a curious and enthusiastic software developer passionate about 
+          <b> AI, web development, systems and backend engineering</b>.  
+        </p>
+      </div>
+    </>
+  );
+}
