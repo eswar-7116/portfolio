e8fc8a67b55db90d5ace39c4a9d6468b2448004a
eswar-7116 <eswardudi06@gmail.com>
2025-09-15 20:01:26 +0530

    fix: remove console errors and add missing meta tag

--- src/components/layout/Contact.tsx
+++ src/components/layout/Contact.tsx
@@ -11,14 +11,15 @@ export default function Contact() {
   return (
     <>
       <div className="flex flex-col sm:flex-row justify-center-safe items-center-safe gap-4 sm:gap-6 text-xl mt-2 px-4 sm:px-0">
-        <Link
+        <a
           href="/Eswar_Dudi.pdf"
           target="_blank"
           className="w-full sm:w-auto border-2 border-blue-950 bg-blue-700 rounded-full cursor-pointer p-3 sm:p-4 flex items-center justify-center gap-2 transition-transform will-change-transform hover:scale-103"
+          rel="noopener noreferrer"
         >
           <FileText />
           Resume
-        </Link>
+        </a>
         <Link
           href="#ama"
           className="w-full sm:w-56 border-2 border-blue-950 bg-blue-700 rounded-full cursor-pointer p-3 sm:p-4 flex items-center justify-center gap-2 transition-transform will-change-transform hover:scale-103"
