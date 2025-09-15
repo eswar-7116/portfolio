a075f2ae4e0b7ddf41f28d85fd6d8cb1fa1d31e5
eswar-7116 <eswardudi06@gmail.com>
2025-09-15 19:21:30 +0530

    fix: escape ' with &apos;

--- src/components/layout/About.tsx
+++ src/components/layout/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <div className="text-xl max-w-3xl mx-auto text-center leading-relaxed space-y-6 mt-16">
         <h1 className="text-4xl font-bold text-blue-500" id="about-heading">About me</h1>
         <p>
-          I'm <b>Eswar Dudi</b>, a software developer passionate about{" "}
+          I&apos;m <b>Eswar Dudi</b>, a software developer passionate about{" "}
           <b> backend systems, AI, and web development</b>. I enjoy building
           awesome projects and learning deep about tech.
         </p>
@@ -27,16 +27,16 @@ export default function About() {
         </p>
         <p>
           I work across multiple languages including Python, Java, JavaScript,
-          TypeScript, C, Go, and Kotlin. I've also built with frameworks and
+          TypeScript, C, Go, and Kotlin. I&apos;ve also built with frameworks and
           stacks like MERN, FastAPI, Flask, Jetpack Compose, and occasionally
           Spring Boot. Beyond tech, I enjoy movies, caring for my plants, and
           long walks with friends.
         </p>
         <p>
-          Currently, I'm pursuing my B.Tech in Computer Science at{" "}
+          Currently, I&apos;m pursuing my B.Tech in Computer Science at{" "}
           <b>CVR College of Engineering</b>. Looking ahead, I aim to grow as a{" "}
           <b>Software Development Engineer</b>, contributing to projects where
-          performance, scalability, and problem-solving are at the core. I'm
+          performance, scalability, and problem-solving are at the core. I&apos;m
           excited to take on opportunities where I can push boundaries in
           domains I love.
         </p>
