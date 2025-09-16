0ce8ecc0b6cb1c18d6dff4a17b4baeb4ecd88f95
eswar-7116 <eswardudi06@gmail.com>
2025-09-16 23:40:28 +0530

    feat: optimize for SEO

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,12 +4,12 @@ import projects from "@/data/projects";
 export default function ProjectsPage() {
   return (
     <>
-      <div className="text-xl max-w-3xl mx-auto mt-12 sm:mt-16 scroll-mt-30 px-4 sm:px-0">
+      <main className="text-xl max-w-3xl mx-auto mt-12 sm:mt-16 scroll-mt-30 px-4 sm:px-0" aria-label="Projects of Eswar Dudi" role="main">
         <h1 className="text-3xl sm:text-4xl font-bold w-full text-center text-blue-500">
           Here are all my {projects.length} projects
         </h1>
-      </div>
-      <div className="flex flex-col items-center-safe justify-center-safe p-4 sm:p-8 gap-4 sm:gap-6">
+      </main>
+      <section className="flex flex-col items-center-safe justify-center-safe p-4 sm:p-8 gap-4 sm:gap-6" id="content">
         {projects.map((proj, i) => (
           <ProjectDetails
             project={proj}
@@ -17,7 +17,7 @@ export default function ProjectsPage() {
             className="bg-blue-900 w-full sm:max-w-3xl p-4 rounded-md"
           />
         ))}
-      </div>
+      </section>
     </>
   );
 }
