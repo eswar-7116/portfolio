2ee30433c80543d1649fa2f4e8359d5959167960
eswar-7116 <eswardudi06@gmail.com>
2025-09-16 21:09:56 +0530

    feat: add all my projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -68,6 +68,41 @@ const projects: Project[] = [
     github: "https://github.com/eswar-7116/NexusChat",
     link: "https://nexuschat-aglp.onrender.com/",
   },
+  {
+    title: "HTTP Server from scratch in C",
+    skills: ["C", "Unix Socket API", "HTTP Protocol"],
+    description: "A minimal HTTP web server built from scratch in C using raw TCP sockets, without any frameworks or libraries. Built to deeply understand how web servers work at the lowest level.",
+    github: "https://github.com/eswar-7116/c-web-server",
+    link: null,
+  },
+  {
+    title: "VoxoLaunch",
+    skills: ["Python", "Speech Recognition", "Text-to-Speech (TTS)"],
+    description: "A Python-based voice-controlled app launcher that executes commands like opening apps, playing videos, and performing searches using speech recognition and TTS.",
+    github: "https://github.com/eswar-7116/VoxoLaunch",
+    link: null,
+  },
+  {
+    title: "python-ffmpeg Docker Image",
+    skills: ["Docker", "Linux", "Python", "FFmpeg"],
+    description: "A lightweight Docker image combining python3-slim with FFmpeg and Git pre-installed, designed for multimedia pipelines and automation workflows.",
+    github: "https://github.com/eswar-7116/python-ffmpeg-docker",
+    link: "https://hub.docker.com/r/eswardudi/python-ffmpeg",
+  },
+  {
+    title: "TermLock",
+    skills: ["Shell Scripting", "Linux"],
+    description: "A lightweight shell script that password-protects terminal startup, blocking escape attempts and ensuring secure access with zero dependencies.",
+    github: "https://github.com/eswar-7116/termlock",
+    link: null,
+  },
+  {
+    title: "Tic-Tac-Toe Android App",
+    skills: ["Android", "Kotlin", "Jetpack Compose", "UI Design", "State Management"],
+    description: "A 2-player TicTacToe Android game built using Kotlin and Jetpack Compose, featuring custom UI and state handling.",
+    github: "https://github.com/eswar-7116/TicTacToe",
+    link: null,
+  }
 ];
 
 export default projects;
