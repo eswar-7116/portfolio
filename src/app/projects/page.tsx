7cf685b4f5584e4477bf5ac6e165e369463f4fd0
Eswar Dudi <eswardudi06@gmail.com>
2025-10-13 19:17:09 +0530

    chore: Improve heading and title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import projects from "@/data/projects";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Eswar Dudi's Projects",
+  title: "Eswar's Projects",
   description: "A showcase of projects built by Eswar Dudi.",
 };
 
@@ -12,7 +12,7 @@ export default function ProjectsPage() {
     <>
       <main className="text-xl max-w-3xl mx-auto mt-12 sm:mt-16 scroll-mt-30 px-4 sm:px-0" aria-label="Projects of Eswar Dudi" role="main">
         <h1 className="text-3xl sm:text-4xl font-bold w-full text-center text-blue-500">
-          Here are all my projects
+          Eswar&apos;s Projects
         </h1>
       </main>
       <section className="flex flex-col items-center-safe justify-center-safe p-4 sm:p-8 gap-4 sm:gap-6" id="content">
