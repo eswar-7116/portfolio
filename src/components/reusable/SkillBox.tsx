73c7c19ae2fd266ab14f65a61017ce719c3f6304
eswar-7116 <eswardudi06@gmail.com>
2025-11-26 15:12:09 +0530

    Feat: use Image from next/image instead of img

--- src/components/reusable/SkillBox.tsx
+++ src/components/reusable/SkillBox.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type SkillBoxProps = { skill: Skill } & React.HTMLAttributes<HTMLDivElement>;
@@ -5,7 +6,7 @@ type SkillBoxProps = { skill: Skill } & React.HTMLAttributes<HTMLDivElement>;
 export default function SkillBox({ skill, ...props }: SkillBoxProps) {
   return (
     <div {...props} className="size-26 bg-gray-700/50 rounded-xl p-2 gap-1 flex flex-col justify-center items-center transition will-change-transform hover:scale-105">
-      <img src={skill.iconURL} alt={skill.title} className="size-10" />
+      <Image src={skill.iconURL} alt={skill.title} height={40} width={40} className="size-10" />
       {skill.title}
     </div>
   );
